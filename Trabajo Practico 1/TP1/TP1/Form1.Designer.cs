5c6adbf2d5c5c7406e1231fa6923abd633a71c39
Chelo <chelobrizuela468@gmail.com>
2020-08-08 16:01:53 -0300

    Punto a

--- Trabajo Practico 1/TP1/TP1/Form1.Designer.cs
+++ Trabajo Practico 1/TP1/TP1/Form1.Designer.cs
@@ -32,15 +32,22 @@
             this.label1 = new System.Windows.Forms.Label();
             this.txt_semilla = new System.Windows.Forms.TextBox();
             this.grdResultado = new System.Windows.Forms.DataGridView();
-            this.txt_m = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.txt_a = new System.Windows.Forms.TextBox();
+            this.txt_m = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.txt_a = new System.Windows.Forms.TextBox();
             this.btnAgregarUno = new System.Windows.Forms.Button();
             this.cboMetodo = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.lbl_c = new System.Windows.Forms.Label();
             this.txt_c = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.lblTiempo = new System.Windows.Forms.Label();
+            this.btnAnterior = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.cboPagina = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.grdResultado)).BeginInit();
             this.SuspendLayout();
             // 
@@ -75,18 +82,11 @@
             this.grdResultado.AllowUserToAddRows = false;
             this.grdResultado.AllowUserToDeleteRows = false;
             this.grdResultado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.grdResultado.Location = new System.Drawing.Point(12, 160);
+            this.grdResultado.Location = new System.Drawing.Point(12, 196);
             this.grdResultado.Name = "grdResultado";
             this.grdResultado.ReadOnly = true;
-            this.grdResultado.Size = new System.Drawing.Size(468, 278);
+            this.grdResultado.Size = new System.Drawing.Size(468, 242);
             this.grdResultado.TabIndex = 3;
-            // 
-            // txt_m
-            // 
-            this.txt_m.Location = new System.Drawing.Point(58, 91);
-            this.txt_m.Name = "txt_m";
-            this.txt_m.Size = new System.Drawing.Size(116, 20);
-            this.txt_m.TabIndex = 5;
             // 
             // label2
             // 
@@ -97,12 +97,12 @@
             this.label2.TabIndex = 4;
             this.label2.Text = "m";
             // 
-            // txt_a
+            // txt_m
             // 
-            this.txt_a.Location = new System.Drawing.Point(58, 65);
-            this.txt_a.Name = "txt_a";
-            this.txt_a.Size = new System.Drawing.Size(116, 20);
-            this.txt_a.TabIndex = 7;
+            this.txt_m.Location = new System.Drawing.Point(58, 91);
+            this.txt_m.Name = "txt_m";
+            this.txt_m.Size = new System.Drawing.Size(116, 20);
+            this.txt_m.TabIndex = 5;
             // 
             // label3
             // 
@@ -113,6 +113,13 @@
             this.label3.TabIndex = 6;
             this.label3.Text = "a";
             // 
+            // txt_a
+            // 
+            this.txt_a.Location = new System.Drawing.Point(58, 65);
+            this.txt_a.Name = "txt_a";
+            this.txt_a.Size = new System.Drawing.Size(116, 20);
+            this.txt_a.TabIndex = 7;
+            // 
             // btnAgregarUno
             // 
             this.btnAgregarUno.Enabled = false;
@@ -122,6 +129,7 @@
             this.btnAgregarUno.TabIndex = 8;
             this.btnAgregarUno.Text = "Agregar";
             this.btnAgregarUno.UseVisualStyleBackColor = true;
+            this.btnAgregarUno.Click += new System.EventHandler(this.btnAgregarUno_Click);
             // 
             // cboMetodo
             // 
@@ -133,6 +141,7 @@
             this.cboMetodo.Name = "cboMetodo";
             this.cboMetodo.Size = new System.Drawing.Size(116, 21);
             this.cboMetodo.TabIndex = 9;
+            this.cboMetodo.SelectedIndexChanged += new System.EventHandler(this.cboMetodo_SelectedIndexChanged);
             // 
             // label4
             // 
@@ -143,6 +152,15 @@
             this.label4.TabIndex = 10;
             this.label4.Text = "Metodo";
             // 
+            // lbl_c
+            // 
+            this.lbl_c.AutoSize = true;
+            this.lbl_c.Location = new System.Drawing.Point(185, 68);
+            this.lbl_c.Name = "lbl_c";
+            this.lbl_c.Size = new System.Drawing.Size(13, 13);
+            this.lbl_c.TabIndex = 11;
+            this.lbl_c.Text = "c";
+            // 
             // txt_c
             // 
             this.txt_c.Location = new System.Drawing.Point(204, 65);
@@ -150,22 +168,81 @@
             this.txt_c.Size = new System.Drawing.Size(116, 20);
             this.txt_c.TabIndex = 12;
             // 
-            // label5
+            // label6
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(185, 68);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(13, 13);
-            this.label5.TabIndex = 11;
-            this.label5.Text = "c";
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(192, 15);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(124, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Cantidad por primera vez";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(322, 12);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(81, 20);
+            this.txtCantidad.TabIndex = 14;
+            this.txtCantidad.Text = "20";
+            // 
+            // lblTiempo
+            // 
+            this.lblTiempo.AutoSize = true;
+            this.lblTiempo.Location = new System.Drawing.Point(185, 94);
+            this.lblTiempo.Name = "lblTiempo";
+            this.lblTiempo.Size = new System.Drawing.Size(42, 13);
+            this.lblTiempo.TabIndex = 15;
+            this.lblTiempo.Text = "Tiempo";
+            // 
+            // btnAnterior
+            // 
+            this.btnAnterior.Location = new System.Drawing.Point(324, 167);
+            this.btnAnterior.Name = "btnAnterior";
+            this.btnAnterior.Size = new System.Drawing.Size(75, 23);
+            this.btnAnterior.TabIndex = 16;
+            this.btnAnterior.Text = "Anterior";
+            this.btnAnterior.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(405, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Siguiente";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // cboPagina
+            // 
+            this.cboPagina.FormattingEnabled = true;
+            this.cboPagina.Location = new System.Drawing.Point(258, 169);
+            this.cboPagina.Name = "cboPagina";
+            this.cboPagina.Size = new System.Drawing.Size(60, 21);
+            this.cboPagina.TabIndex = 18;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(212, 172);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Pagina";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(495, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cboPagina);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnAnterior);
+            this.Controls.Add(this.lblTiempo);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.txt_c);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbl_c);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.cboMetodo);
             this.Controls.Add(this.btnAgregarUno);
@@ -191,15 +268,22 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txt_semilla;
         private System.Windows.Forms.DataGridView grdResultado;
-        private System.Windows.Forms.TextBox txt_m;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txt_a;
+        private System.Windows.Forms.TextBox txt_m;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_a;
         private System.Windows.Forms.Button btnAgregarUno;
         private System.Windows.Forms.ComboBox cboMetodo;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_c;
         private System.Windows.Forms.TextBox txt_c;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label lblTiempo;
+        private System.Windows.Forms.Button btnAnterior;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox cboPagina;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- Trabajo Practico 1/TP1/TP1/Form1.cs
+++ Trabajo Practico 1/TP1/TP1/Form1.cs
@@ -7,33 +7,183 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TP1.Clases;
 
 namespace TP1
 {
     public partial class Form1 : Form
     {
+        List<string> numeros = new List<string>();
+        Generador generador = new Generador();
+
+        double Xn = 0;
+
         public Form1()
         {
             InitializeComponent();
+
+            //Semilla = 37
+            //a = 91
+            //c = 43
+            //m = 100
+
+            txt_semilla.Text = "37";
+            txt_a.Text = "91";
+            txt_c.Text = "43";
+            txt_m.Text = "100";
+
         }
 
         private void btnCalcular_Click(object sender, EventArgs e)
         {
+            numeros = new List<string>();
+
+            DateTime fechaDesde = DateTime.Now;
             //Congruencial Mixto
             //Xn+1 = (a*Xn + c) mod m (%)
 
             //Congruencial Multiplicativo
             //Xn+1 = (a * Xn) mod m (%)
 
-            if(cboMetodo.SelectedItem.ToString() == "Congruencial Multiplicativo")
-            {
+            Xn = 0;
 
+            int cantidadPorPrimeraVez = int.Parse(txtCantidad.Text);
+
+            double siguienteValor = 0;
+            double numeroGenerado = 0;
+            
+            double a = double.Parse(txt_a.Text);
+            double m = double.Parse(txt_m.Text);
+            double c = 0;
+
+            if (!string.IsNullOrEmpty(txt_c.Text))
+                c = double.Parse(txt_c.Text);
+
+
+            string metodoSeleccionado = cboMetodo.SelectedItem.ToString();
+
+                for (int i = 0; i < cantidadPorPrimeraVez; i++)
+                {
+                    if (i == 0)
+                    {
+                        Xn = double.Parse(txt_semilla.Text);
+                    }
+
+                    if (metodoSeleccionado == "Congruencial Multiplicativo")
+                    {
+                        siguienteValor = generarMultiplicativo(Xn, a, m);
+                    }
+                    else
+                    {
+                        siguienteValor = generarMixto(Xn, a, m, c);
+                    }
+
+                    Xn = siguienteValor;
+
+                    numeroGenerado = siguienteValor / (m - 1);
+
+                    numeroGenerado = Math.Truncate(numeroGenerado * 10000) / 10000;
+
+                    numeros.Add(numeroGenerado.ToString("0.0000"));
+                    
+                }            
+
+            cargarGrilla();
+
+            btnAgregarUno.Enabled = true;
+
+            DateTime fechaHasta = DateTime.Now;
+
+            TimeSpan ts = fechaHasta - fechaDesde;
+
+            lblTiempo.Text = ts.Minutes.ToString() + ":" + ts.Seconds.ToString() + " mm:ss";
+
+        }
+
+        private double generarMixto(double Xn, double a, double m, double c)
+        {
+            double resultado = generador.generarMixto(Xn, a, m, c);
+
+            return resultado;
+        }
+
+        private double generarMultiplicativo(double Xn, double a, double m)
+        {
+            double resultado = generador.generarMultiplicativo(Xn, a, m);
+
+            return resultado;
+        }
+
+        private void btnAgregarUno_Click(object sender, EventArgs e)
+        {
+            double siguienteValor = 0;
+            double a = double.Parse(txt_a.Text);
+            double m = double.Parse(txt_m.Text);
+            double c = double.Parse(txt_c.Text);
+            double numeroGenerado = 0;
+
+            if (cboMetodo.SelectedItem.ToString() == "Congruencial Multiplicativo")
+            {
+                siguienteValor = generarMultiplicativo(Xn, a, m);
             }
             else
             {
-
+                siguienteValor = generarMixto(Xn, a, m, c);                
             }
 
+            Xn = siguienteValor;
+
+            numeroGenerado = siguienteValor / (m - 1);
+
+            numeroGenerado = Math.Truncate(numeroGenerado * 10000) / 10000;
+
+            numeros.Add(numeroGenerado.ToString("0.0000"));
+
+
+            cargarGrilla();
+
+            
+
+        }
+
+        private void cargarGrilla()
+        {
+            DataTable table = new DataTable();
+
+            table.Columns.Add("Indice");
+            table.Columns.Add("Numeros Aleatorios");
+
+            int indice = 1;            
+
+            foreach (var numero in numeros)   
+            {
+                DataRow fila = table.NewRow();
+
+                fila["Indice"] = indice;
+                fila["Numeros Aleatorios"] = numero;
+
+                table.Rows.Add(fila);
+
+                indice++;
+            }
+
+            grdResultado.DataSource = table;
+        }
+
+        private void cboMetodo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(cboMetodo.SelectedItem.ToString() == "Congruencial Multiplicativo")
+            {
+                txt_c.Text = "";
+                txt_c.Visible = false;
+                lbl_c.Visible = false;
+            }
+            else
+            {
+                txt_c.Text = "";
+                txt_c.Visible = true;
+                lbl_c.Visible = true;
+            }
         }
     }
 }
